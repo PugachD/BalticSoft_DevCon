46df6e43bca72a016eb54a9ce12f4825f126b4ae
Dmitriy <pugachev15@gmail.com>
2016-05-24 09:10:03 +0300

    no message

--- BalticSoft_DevTest/Handler.cs
+++ BalticSoft_DevTest/Handler.cs
@@ -1,42 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace BalticSoft_DevTest
 {
     public class Handler
     {
-        DBWork dbWork { get; set; }
         Order order { get; set; }
 
         public Handler()
         {
-            dbWork = new DBWork();
             order = null;
         }
-        
+
         public void LoadOrder()
         {
             int ID = EnterData.IDOrder();
-            order = dbWork.LoadOrder(ID);
+            order = DBWork.LoadOrder(ID);
 
             Show.ShowOrder(order);
         }
 
         public void UpdateOrder()
         {
-
             int ID = EnterData.IDOrder();
-            dbWork.ChangeOrder(ID);
+            order = DBWork.LoadOrder(ID);
+            if (order != null)
+            {
+                Console.WriteLine("Новые данные заказа\n");
+                order = ReturnTypeOrder(!order.IDSupplier.IsNull);
 
+                GoMethod();
+                EnterData.EnterDataOrder(order);
+                //Изменяем общую сумму заказа
+                order.Process();
+                order.IDOrder = ID;
+                DBWork.UpdateOrder(order);
+            }
+            Show.ShowOrder(order);
         }
 
         public void CreateOrder()
         {
             int who = EnterData.ChoiceCustomer();
-            dbWork.CreateOrder(who);
+
+            order = ReturnTypeOrder(who == 1);
+
+            GoMethod();
+
+            EnterData.EnterDataOrder(order);
+            //Изменяем общую сумму заказа
+            order.Process();
+
+            DBWork.InsertOrderInDB(order);
+            Show.ShowOrder(order);
+        }
+
+        private void GoMethod()
+        {
+            //Вызывается метод с именем Create + именем класса типа переменной для создания нового элемента этого типа заказа
+            MethodInfo Method = Type.GetType("BalticSoft_DevTest.DBWork").GetMethod("Create" + order.GetType().Name);
+            Method.Invoke(null, new Object[] { order });
+        }
+
+        private Order ReturnTypeOrder(bool b)
+        {
+            Order ord = null;
+            if (b)
+            {
+                ord = new OrderFromBuyer();
+            }
+            else
+            {
+                ord = new OrderToTheSupplier();
+            }
+            return ord;
         }
     }
 }
